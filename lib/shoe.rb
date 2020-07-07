b501d20e924b660f997f4ada497fe0467d9f96a1
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-07 02:36:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,4 +11,50 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
 end
